e2e93009bb0bb0146f201ef202a4b19398ec488f
Andre Kuo <kansetsu4@gmail.com>
2022-09-10 20:44:15 +0800

    Add github link

--- src/awesome_name/views/main.cljs
+++ src/awesome_name/views/main.cljs
@@ -1,23 +1,27 @@
 (ns awesome-name.views.main
   (:require
     [awesome-name.component.core :as cpt]
-    [awesome-name.subs :as sub]
     [awesome-name.events :as evt]
+    [awesome-name.subs :as sub]
     [awesome-name.views.combinations :as combinations]
     [awesome-name.views.evaluation :as evaluation]
+    [re-frame.core :as rf]
     [reagent-mui.components :as mui]
-    [re-frame.core :as rf]))
+    [reagent-mui.icons.git-hub :as icon-github]))
 
 (defn main
   []
   (let [current-page @(rf/subscribe [::sub/current-page])]
-   [mui/grid {:container true :spacing 2}
-    [mui/grid {:item true :xs 12}
-     [cpt/tab-context {:value current-page}
-      [cpt/tab-list {:on-change #(rf/dispatch-sync [::evt/set-page %2])}
-       [mui/tab {:label "姓名組合" :value "combinations"}]
-       [mui/tab {:label "姓名評分" :value "evaluation"}]]
-      [cpt/tab-panel {:value "combinations"}
-       [combinations/main]]
-      [cpt/tab-panel {:value "evaluation"}
-       [evaluation/main]]]]]))
+    [mui/grid {:container true :spacing 2}
+     [mui/grid {:item true :xs 12}
+      [cpt/tab-context {:value current-page}
+       [cpt/tab-list {:on-change #(rf/dispatch-sync [::evt/set-page %2])}
+        [mui/tab {:label "姓名組合" :value "combinations"}]
+        [mui/tab {:label "姓名評分" :value "evaluation"}]]
+       [cpt/tab-panel {:value "combinations"}
+        [combinations/main]]
+       [cpt/tab-panel {:value "evaluation"}
+        [evaluation/main]]]
+      [mui/box {:sx {:position :fixed :right 5 :top 5}}
+       [:a {:href "https://kansetsu7.github.io/awesome_name/"}
+        [icon-github/git-hub]]]]]))
