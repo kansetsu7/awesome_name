5eec21cc4fe96f9ae6ddbdd3e3b293b62363811d
Andre Kuo <kansetsu4@gmail.com>
2022-11-01 15:56:34 +0800

    Add faq

--- src/awesome_name/views/main.cljs
+++ src/awesome_name/views/main.cljs
@@ -5,6 +5,7 @@
     [awesome-name.subs :as sub]
     [awesome-name.views.combinations :as combinations]
     [awesome-name.views.evaluation :as evaluation]
+    [awesome-name.views.faq :as faq]
     [re-frame.core :as rf]
     [reagent-mui.components :as mui]
     [reagent-mui.icons.git-hub :as icon-github]))
@@ -17,11 +18,14 @@
       [cpt/tab-context {:value current-page}
        [cpt/tab-list {:on-change #(rf/dispatch-sync [::evt/set-page %2])}
         [mui/tab {:label "姓名組合" :value "combinations"}]
-        [mui/tab {:label "姓名評分" :value "evaluation"}]]
+        [mui/tab {:label "姓名評分" :value "evaluation"}]
+        [mui/tab {:label "常見問題" :value "faq"}]]
        [cpt/tab-panel {:value "combinations"}
         [combinations/main]]
        [cpt/tab-panel {:value "evaluation"}
-        [evaluation/main]]]
+        [evaluation/main]]
+       [cpt/tab-panel {:value "faq"}
+        [faq/main]]]
       [mui/box {:sx {:position :fixed :right 5 :top 5}}
        [:a {:href "https://github.com/kansetsu7/awesome_name"}
         [icon-github/git-hub]]]]]))
